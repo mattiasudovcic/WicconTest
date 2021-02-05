8bcc8b79f24a4b54f167b5654ec66a8c160feb4b
mattiasudovcic <mattias.udovcic@gmail.com>
2021-02-05 11:50:03 +0100

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WicconTest.Startup))]
+namespace WicconTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
